1c1c2371560b05c5c02f7d7bfee7512d6901dddc
미니마니모 <@HORNET14>
2024-06-05 17:07:00 +0900

    ~ 섹션 2 - 12단계까지

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -1,8 +1,50 @@
 package com.in28minutes.learnspringframework;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+// Released in JDK 16.
+// record : 세터, 게터 등 생성자를 만들 필요 X
+record Person(String name, int age, Address address) { };
+
+record Address(String firstLine, String city) { };
 
 @Configuration
 public class HelloWorldConfiguration {
+
+	@Bean
+	public String name() {
+		return "Ranga";
+	}
+
+	@Bean
+	public int age() {
+		return 15;
+	}
+
+	@Bean
+	public Person person() {
+		return new Person("Ravi", 20, new Address("Main Street", "Utrecht"));
+	}
 	
+	@Bean
+	public Person person2MethodCall() { // Method
+		return new Person(name(), age(), address());
+	}
+	
+	@Bean
+	public Person person3Parameters(String name, int age, Address address3) { // Parameters
+		return new Person(name, age, address3);
+	}
+	
+	@Bean(name = "address2")
+	public Address address() {
+		return new Address("Baker Street", "London");
+	}
+	
+	@Bean(name = "address3")
+	public Address address3() {
+		return new Address("Motinagar", "Hyderabad");
+	}
+
 }
